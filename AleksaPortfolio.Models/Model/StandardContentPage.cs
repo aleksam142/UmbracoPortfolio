31544ab7d20c51ebb3f2b53f200006f8de4ffe94
aleksam142 <aleksa.marinkovic10@gmail.com>
2024-12-29 20:48:15 +0100

    Added form module with bootstrap. Registered API key.

---
+++ AleksaPortfolio.Models/Model/StandardContentPage.cs
@@ -0,0 +1,13 @@
+﻿using AleksaPortfolio.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AleksaPortfolio.Models.Model
+{
+    public partial class StandardContentPage : IPage
+    {
+    }
+}
